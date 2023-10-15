24c76dc3520f86d7a5f65a4b1a549a63be312be2
Azamat Rasulov <azamat@Azamats-MacBook-Pro.local>
2023-10-15 14:39:43 +0900

    creating pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,35 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
 
 export default function App() {
-  return <div className='text-red-500'>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path='/'
+          element={<Home />}
+        />
+        <Route
+          path='/sign-up'
+          element={<SignUp />}
+        />
+        <Route
+          path='/sign-in'
+          element={<SignIn />}
+        />
+        <Route
+          path='/about'
+          element={<About />}
+        />
+        <Route
+          path='/profile'
+          element={<Profile />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
